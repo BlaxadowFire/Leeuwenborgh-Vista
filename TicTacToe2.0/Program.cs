ef43986ea861fe93ef503fd00dc4f6a2936a92db
BlaxadowFire <nando.kools116325@student.leeuwenborgh.nl>
2017-09-27 13:14:11 +0200

    Added the option for the user to enter their name. Also made the program ask if the players want to restart.

--- TicTacToe2.0/Program.cs
+++ TicTacToe2.0/Program.cs
@@ -8,8 +8,341 @@ namespace TicTacToe2._0
 {
     class Program
     {
-        static void Main(string[] args)
+        public static string A = "1";
+        public static string B = "2";
+        public static string C = "3";
+        public static string D = "4";
+        public static string E = "5";
+        public static string F = "6";
+        public static string G = "7";
+        public static string H = "8";
+        public static string I = "9";
+
+        public static string strPlayer1;
+        public static string strPlayer2;
+        public static string strCurrentPlayer;
+        public static bool blnGameOVer = false;
+        public static string strCPChoice;
+        public static string strUserInput = string.Empty;
+
+        public static string restart = "y";
+
+        public static bool firstrun = true;
+
+        static void Main()
         {
+            do
+            {
+                if (firstrun == true)
+                {
+                    Console.Title = "TicTacToe By Nando";
+                    Console.WriteLine("Player1, enter your name:");
+                    strPlayer1 = Console.ReadLine();
+                    Console.WriteLine("Player2, enter your name:");
+                    strPlayer2 = Console.ReadLine();
+                    strCurrentPlayer = strPlayer1;
+                    firstrun = false;
+                }
+                do
+                {
+                    Program.drawgrid();
+                    Program.strUserInput = Console.ReadLine();
+                    Program.TurnCheck();
+                    Program.Turn();
+                    Program.checkifwon();
+                    Program.SwitchTurn();
+                }
+                while (blnGameOVer == false);
+                Console.WriteLine("It's a tie!");
+                Console.WriteLine("Do you want to play again? y/n");
+                restart = Console.ReadLine();
+                if (restart == "y")
+                {
+                    Program.funcrestart();
+                }
+                else
+                {
+                    Environment.Exit(0);
+                }
+            }
+            while (restart == "y");
+            Environment.Exit(0);
+
+            
+        }
+        public static void TurnCheck()
+        {
+            if (strCurrentPlayer == strPlayer1)
+            {
+                strCPChoice = "X";
+            }
+            else
+            {
+                strCPChoice = "O";
+            }
+        }
+        public static void Turn()
+        {
+            Console.Clear();
+            switch(strUserInput)
+            {
+                case "1":
+                    {
+                        if (A == "1")
+                        {
+                            A = strCPChoice;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error, this one is already taken");
+                            Console.ReadLine();
+                            Program.Main();
+                        }
+                        break;
+                    }
+                case "2":
+                    {
+                        if (B == "2")
+                        {
+                            B = strCPChoice;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error, this one is already taken");
+                            Console.ReadLine();
+                            Program.Main();
+                        }
+                        break;
+                    }
+                case "3":
+                    {
+                        if (C == "3")
+                        {
+                            C = strCPChoice;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error, this one is already taken");
+                            Console.ReadLine();
+                            Program.Main();
+                        }
+                        break;
+                    }
+                case "4":
+                    {
+                        if (D == "4")
+                        {
+                            D = strCPChoice;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error, this one is already taken");
+                            Console.ReadLine();
+                            Program.Main();
+                        }
+                        break;
+                    }
+                case "5":
+                    {
+                        if (E == "5")
+                        {
+                            E = strCPChoice;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error, this one is already taken");
+                            Console.ReadLine();
+                            Program.Main();
+                        }
+                        break;
+                    }
+                case "6":
+                    {
+                        if (F == "6")
+                        {
+                            F = strCPChoice;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error, this one is already taken");
+                            Console.ReadLine();
+                            Program.Main();
+                        }
+                        break;
+                    }
+                case "7":
+                    {
+                        if (G == "7")
+                        {
+                            G = strCPChoice;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error, this one is already taken");
+                            Console.ReadLine();
+                            Program.Main();
+                        }
+                        break;
+                    }
+                case "8":
+                    {
+                        if (H == "8")
+                        {
+                            H = strCPChoice;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error, this one is already taken");
+                            Console.ReadLine();
+                            Program.Main();
+                        }
+                        break;
+                    }
+                case "9":
+                    {
+                        if (I == "9")
+                        {
+                            I = strCPChoice;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error, this one is already taken");
+                            Console.ReadLine();
+                            Program.Main();
+                        }
+                        break;
+                    }
+                case "bomboozle":
+                    A = strCPChoice;
+                    B = strCPChoice;
+                    C = strCPChoice;
+                    D = strCPChoice;
+                    E = strCPChoice;
+                    F = strCPChoice;
+                    G = strCPChoice;
+                    H = strCPChoice;
+                    I = strCPChoice;
+                    Console.Clear();
+                    Program.drawgrid();
+                    Console.ReadLine();
+
+                    break;
+                default:
+                    {
+                        Console.WriteLine("Error, give valid input");
+                        Console.ReadLine();
+                        Program.Main();
+                        break;
+                    }
+            }
+        }
+        public static void SwitchTurn()
+        {
+            if (strCurrentPlayer == strPlayer1)
+            {
+                strCurrentPlayer = strPlayer2;
+            }
+            else
+            {
+                strCurrentPlayer = strPlayer1;
+            }
+        }
+        public static void checkifwon()
+        {
+            if (A == B && B == C)
+            {
+                Program.won();
+            }
+            else if (D == E && E == F)
+            {
+                Program.won();
+            }
+            else if (G == H && H == I)
+            {
+                Program.won();
+            }
+            else if (A == D && D == G)
+            {
+                Program.won();
+            }
+            else if (B == E && E == H)
+            {
+                Program.won();
+            }
+            else if (C == F && F == I)
+            {
+                Program.won();
+            }
+            else if (A == E && E == I)
+            {
+                Program.won();
+            }
+            else if (C == E && E == G)
+            {
+                Program.won();
+            }
+            if (A != "1" && B != "2" && C != "3" && D != "4" && E != "5" && F != "6" && G != "7" && H != "8" && I != "9")
+            {
+                blnGameOVer = true;
+            }
+        }
+        public static void drawgrid()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("{0} Give input\r\n\r\n", strCurrentPlayer);
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("{0} | {1} | {2} ", A, B, C);
+            Console.WriteLine("----------");
+            Console.WriteLine("{0} | {1} | {2} ", D, E, F);
+            Console.WriteLine("----------");
+            Console.WriteLine("{0} | {1} | {2} ", G, H, I);
+            Console.WriteLine("----------\r\n");
+        }
+        public static void won()
+        {
+            Console.WriteLine("Congratulations {0}, you won!", strCurrentPlayer);
+            Console.WriteLine("Do you want to play again? y/n");
+            restart = Console.ReadLine();
+            if (restart == "y")
+            {
+                Program.funcrestart();
+            }
+            else
+            {
+            Environment.Exit(0);
+            }
+        }
+        public static void funcrestart()
+        {
+            A = "1";
+            B = "2";
+            C = "3";
+            D = "4";
+            E = "5";
+            F = "6";
+            G = "7";
+            H = "8";
+            I = "9";
+
+            strPlayer1 = string.Empty;
+            strPlayer2 = string.Empty;
+            strCurrentPlayer = string.Empty;
+            blnGameOVer = false;
+            strCPChoice = string.Empty;
+            strUserInput = string.Empty;
+            firstrun = true;
+            Console.Clear();
+
+            if (restart == "n")
+            {
+                Environment.Exit(0);
+            }
+            else
+            {
+                Program.Main();
+            }
         }
     }
 }
