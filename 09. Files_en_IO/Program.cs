b3f9f3a62a4fcdcaa914f4f79d441ecaca324f14
BlaxadowFire <nando.kools116325@student.leeuwenborgh.nl>
2017-10-02 17:35:06 +0200

    Added an edit option, and made sure that the user needs to give valid input.

--- 09. Files_en_IO/Program.cs
+++ 09. Files_en_IO/Program.cs
@@ -12,7 +12,7 @@ namespace _09.Files_en_IO
         public static string strFile = "UserInput.txt";
         public static string strPath = "../../";
         public static string strText = string.Empty;
-        static void Main(string[] args)
+        static void Main()
         {
             Console.Title = "Made by Nando";
             do
@@ -54,9 +54,22 @@ namespace _09.Files_en_IO
         }
         public static void funWrite()
         {
+            labWrite:
             Console.Clear();
-            Console.WriteLine("Hoeveel regels wilt u wegscrhijven?");
-            int intRegels = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hoeveel regels wilt u wegscrhijven?\r\n(0 om terug te gaan)");
+            string strRegels = Console.ReadLine();
+            int intRegels;
+            if (!Int32.TryParse(strRegels, out intRegels))
+            {
+                Console.WriteLine("Please enter a valid value.");
+                Console.ReadLine();
+                goto labWrite;
+            }
+            if (intRegels == 0)
+            {
+                Program.Main();
+            }
+
             Console.Clear();
             Console.WriteLine("Schrijf de regels die u in het bestand wilt schrijven.");
             string[] strWrite = { };
@@ -76,7 +89,37 @@ namespace _09.Files_en_IO
         }
         public static void funEdit()
         {
+            labUserEdit:
+            Console.Clear();
+            string[] strText = File.ReadAllLines(strPath + strFile);
+            for (int i = 0; i < strText.Length; i++)
+            {
+                Console.WriteLine("{0}.| {1}",i+1, strText[i]);
+            }
+            Console.WriteLine("\r\nGeef aan welke regel u wilt bewerken\r\n(1 regel tegelijkertijd)\r\n(0 om terug te gaan)");
+            //int intUserEdit = Convert.ToInt32(Console.ReadLine());
+            string strUserEdit = Console.ReadLine();
+            int intUserEdit;
+            if (!Int32.TryParse(strUserEdit, out intUserEdit))
+            {
+                Console.WriteLine("Please enter a valid value.");
+                Console.ReadLine();
+                goto labUserEdit;
+            }
+            if (intUserEdit ==0)
+            {
+                Program.Main();
+            }
 
+
+            intUserEdit -= 1;
+            Console.Clear();
+            Console.WriteLine(strText[intUserEdit]);
+            Console.WriteLine("Geef aan welke text u hier wilt hebben staan.");
+            strText[intUserEdit] = Console.ReadLine();
+            File.WriteAllLines(strPath + strFile, strText);
+            Console.WriteLine("Done");
+            Console.ReadLine();
         }
     }
 }
