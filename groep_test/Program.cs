79ca82754c3dfe18721190758b409c1279fb4fbe
Tom Bastian <tom.bastian204001@student.leeuwenborgh.nl>
2017-10-04 10:58:09 +0200

    ik heb een writeline en een readline aangemaakt

--- groep_test/Program.cs
+++ groep_test/Program.cs
@@ -10,6 +10,8 @@ namespace groep_test
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("alles goed met jullie?");
+            Console.ReadLine();
             //test
         }
     }
