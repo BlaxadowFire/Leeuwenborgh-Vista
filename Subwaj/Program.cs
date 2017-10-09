26993a2c2f664e80a7493bca32b815889d2da71e
BlaxadowFire <nando.kools116325@student.leeuwenborgh.nl>
2017-10-09 16:51:36 +0200

    Added a start menu, rooms, halls, errors.

--- Subwaj/Program.cs
+++ Subwaj/Program.cs
@@ -3,13 +3,490 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Subwaj
 {
     class Program
     {
+        //Here we will place the public static variables
+        public static Random _randomforeground = new Random();  //Gets used for random foregroundcolor.
+        public static ConsoleColor originalForegroundColor;     //Sets the old foreground to a variable to make sure it isn't the same.
+
+        public static ConsoleKeyInfo cki; //uses cki to use readkey.
+        public static string CurrentRoom = string.Empty; //Makes sure the program knows in what room the user is.
+
+        //Makes it easier to change rooms
+        public static string strMainMenu = "MainMenu";
+        public static string strInGameMenu = "InGameMenu";
+        public static string strROOM1 = "ROOM1";
+        public static string strROOM2 = "ROOM2";
+        public static string strROOM3 = "ROOM3";
+        public static string strROOM4 = "ROOM4";
+        public static string strROOM5 = "ROOM5";
+        public static string strROOM6 = "ROOM6";
+        public static string strROOM7 = "ROOM7";
+        public static string strHALL1 = "HALL1";
+        public static string strHALL2 = "HALL2";
+        public static string strHALL3 = "HALL3";
+        public static string strHALL4 = "HALL4";
+        public static string strHALL5 = "HALL5";
+        public static string strHALL6 = "HALL6";
+        public static string strHALL7 = "HALL7";
+
+
         static void Main(string[] args)
         {
+            Console.Title = "NOT A GAME";
+            //Loops the program
+            do
+            {
+                Program.MAINMENU();
+            }
+            while (true);
+        }
+
+        //Checks UserInput
+        public static void UserInput(ConsoleKeyInfo cki)
+        {
+            string strCKI = cki.Key.ToString();
+            switch (CurrentRoom)
+            {
+                case "MainMenu":
+                    {
+                        switch (strCKI)
+                        {
+                            case "NumPad1":
+                            case "D1":
+                                {
+                                    Program.MainMenuStart();
+                                    break;
+                                }
+
+                            case "NumPad2":
+                            case "D2":
+                                {
+                                    Program.MainMenuHelp();
+                                    break;
+                                }
+
+                            case "NumPad3":
+                            case "D3":
+                                {
+                                    Program.MainMenuOptions();
+                                    break;
+                                }
+
+                            case "NumPad4":
+                            case "D4":
+                                {
+                                    Program.MainMenuCode();
+                                    break;
+                                }
+
+                            case "NumPad5":
+                            case "D5":
+                                {
+                                    Program.MainMenuAchievements();
+                                    break;
+                                }
+
+                            case "NumPad6":
+                            case "D6":
+                                {
+                                    Program.MainMenuLoadSaveGame();
+                                    break;
+                                }
+
+                            case "NumPad0":
+                            case "D0":
+                                {
+                                    Program.MainMenuExit();
+                                    break;
+                                }
+
+                            default:
+                                {
+                                    Program.MAINMENU();
+                                    break;
+                                }
+
+                            case "Oem3":
+                                {
+                                    Console.Clear();
+                                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                                    Console.WriteLine("ControlKeyInfo output debug");
+                                    Console.BackgroundColor = ConsoleColor.Black;
+                                    Console.ForegroundColor = ConsoleColor.White;
+                                    do
+                                    {
+
+                                        cki = Console.ReadKey();
+                                        strCKI = cki.Key.ToString();
+                                        Console.WriteLine("");
+                                        Console.WriteLine("OUTPUT:" + strCKI);
+                                    } while (true);
+                                }
+
+                        }
+                        break;
+                    }
+                case "InGameMenu":
+                    {
+                        switch (strCKI)
+                        {
+                            default:
+                                {
+                                    break;
+                                }
+                        }
+                        break;
+                    }
+                case "ROOM1":
+                    {
+                        switch (strCKI)
+                        {
+                            default:
+                                {
+                                    break;
+                                }
+                        }
+                        break;
+                    }
+                case "ROOM2":
+                    {
+                        switch (strCKI)
+                        {
+                            default:
+                                {
+                                    break;
+                                }
+                        }
+                        break;
+                    }
+                case "ROOM3":
+                    {
+                        switch (strCKI)
+                        {
+                            default:
+                                {
+                                    break;
+                                }
+                        }
+                        break;
+                    }
+                case "ROOM4":
+                    {
+                        switch (strCKI)
+                        {
+                            default:
+                                {
+                                    break;
+                                }
+                        }
+                        break;
+                    }
+                case "ROOM5":
+                    {
+                        switch (strCKI)
+                        {
+                            default:
+                                {
+                                    break;
+                                }
+                        }
+                        break;
+                    }
+                case "ROOM6":
+                    {
+                        switch (strCKI)
+                        {
+                            default:
+                                {
+                                    break;
+                                }
+                        }
+                        break;
+                    }
+                case "ROOM7":
+                    {
+                        switch (strCKI)
+                        {
+                            default:
+                                {
+                                    break;
+                                }
+                        }
+                        break;
+                    }
+                case "HALL1":
+                    {
+                        switch (strCKI)
+                        {
+                            default:
+                                {
+                                    break;
+                                }
+                        }
+                        break;
+                    }
+                case "HALL2":
+                    {
+                        switch (strCKI)
+                        {
+                            default:
+                                {
+                                    break;
+                                }
+                        }
+                        break;
+                    }
+                case "HALL3":
+                    {
+                        switch (strCKI)
+                        {
+                            default:
+                                {
+                                    break;
+                                }
+                        }
+                        break;
+                    }
+                case "HALL4":
+                    {
+                        switch (strCKI)
+                        {
+                            default:
+                                {
+                                    break;
+                                }
+                        }
+                        break;
+                    }
+                case "HALL5":
+                    {
+                        switch (strCKI)
+                        {
+                            default:
+                                {
+                                    break;
+                                }
+                        }
+                        break;
+                    }
+                case "HALL6":
+                    {
+                        switch (strCKI)
+                        {
+                            default:
+                                {
+                                    break;
+                                }
+                        }
+                        break;
+                    }
+                case "HALL7":
+                    {
+                        switch (strCKI)
+                        {
+                            default:
+                                {
+                                    break;
+                                }
+                        }
+                        break;
+                    }
+                default:
+                    {
+                        Program.ErrorHandlerStart();
+                        Console.WriteLine("ERROR: ROOM NOT KNOWN");
+                        Console.WriteLine("ERROR ID:0001");
+                        Program.ErrorFinisher();
+                        break;
+                    }
+            }
+            Program.ErrorHandlerStart();
+            Console.WriteLine("ERROR: OUT OF ROOM EXCEPTION");
+            Console.WriteLine("ERROR ID: 0002");
+            Program.ErrorFinisher();
+        }
+
+        // BEGIN OF MAINMENU
+        public static void MAINMENU()
+        {
+            Console.Clear();
+            //Loops the main menu.
+            do
+            {
+                CurrentRoom = "MainMenu";
+
+                //makes the text a random color and prevents the foregroundcolor to be the same as the backgroundcolor.
+                do
+                {
+                    originalForegroundColor = Console.ForegroundColor;
+                    Console.ForegroundColor = GetRandomConsoleColor();
+                }
+                while (Console.ForegroundColor == Console.BackgroundColor || Console.ForegroundColor == originalForegroundColor);
+                Console.Clear();
+                Console.WriteLine("\t\t\t\t\tMade by Nando, Shunhui, Julean, Tom\r\n" +
+                    "\t\t\t\t\t\tPresenting\r\n" +
+                    "\t\t\t\t\t\tNOT A GAME\r\n\r\n" +
+                    "1.)\tStart\r\n" +
+                    "2.)\tHelp\r\n" +
+                    "3.)\tOptions\r\n" +
+                    "4.)\tCode\r\n" +
+                    "5.)\tAchievements\r\n" +
+                    "6.)\tLoad Save Game\r\n" +
+                    "0.)\tExit");
+
+                Console.ForegroundColor = ConsoleColor.White;
+                cki = Console.ReadKey();
+                Program.UserInput(cki);
+
+            } while (true);
+        }
+        public static void MainMenuStart()
+        {
+            //story
+            Program.ROOM1();
+        }
+        public static void MainMenuHelp()
+        {
+            Program.ErrorNotYetCreated();
+            Program.MAINMENU();
+        }
+        public static void MainMenuOptions()
+        {
+            Program.ErrorNotYetCreated();
+            Program.MAINMENU();
+        }
+        public static void MainMenuCode()
+        {
+            Program.ErrorNotYetCreated();
+            Program.MAINMENU();
+        }
+        public static void MainMenuAchievements()
+        {
+            Program.ErrorNotYetCreated();
+            Program.MAINMENU();
+        }
+        public static void MainMenuLoadSaveGame()
+        {
+            Program.ErrorNotYetCreated();
+            Program.MAINMENU();
+        }
+        public static void MainMenuExit()
+        {
+            Program.ErrorNotYetCreated();
+            Program.MAINMENU();
+        }
+        //END OF MAINMENU
+
+
+        //BEGIN OF ROOMS
+        public static void ROOM1()
+        {
+            CurrentRoom = strROOM1;
+            Program.ErrorNotYetCreated();
+        }
+        public static void ROOM2()
+        {
+            CurrentRoom = strROOM2;
+        }
+        public static void ROOM3()
+        {
+            CurrentRoom = strROOM3;
+        }
+        public static void ROOM4()
+        {
+            CurrentRoom = strROOM5;
+        }
+        public static void ROOM5()
+        {
+            CurrentRoom = strROOM5;
+        }
+        public static void ROOM6()
+        {
+            CurrentRoom = strROOM6;
+        }
+        public static void ROOM7()
+        {
+            CurrentRoom = strROOM7;
+        }
+        //END OF ROOMS
+
+        //BEGIN OF HALLS
+        public static void HALL1()
+        {
+            CurrentRoom = strHALL1;
+        }
+        public static void HALL2()
+        {
+            CurrentRoom = strHALL2;
+        }
+        public static void HALL3()
+        {
+            CurrentRoom = strHALL3;
+        }
+        public static void HALL4()
+        {
+            CurrentRoom = strHALL4;
+        }
+        public static void HALL5()
+        {
+            CurrentRoom = strHALL5;
+        }
+        public static void HALL6()
+        {
+            CurrentRoom = strHALL6;
+        }
+        public static void HALL7()
+        {
+            CurrentRoom = strHALL7;
+        }
+        //END OF HALLS
+
+        //Random ConsoleColor Generator
+        public static ConsoleColor GetRandomConsoleColor()
+        {
+            Array consoleColors = Enum.GetValues(typeof(ConsoleColor));
+            return (ConsoleColor)consoleColors.GetValue(_randomforeground.Next(consoleColors.Length));
+        }
+
+
+        //Error color
+        public static void ErrorHandlerStart()
+            {
+                Console.BackgroundColor = ConsoleColor.DarkYellow;
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.BackgroundColor = ConsoleColor.Yellow;
+            }
+        //Error preventor
+        public static void ErrorFinisher()
+        {
+            Console.WriteLine("\r\nInformation:\r\nCurrentRoom = {0}\r\n", CurrentRoom);
+            Console.WriteLine("An unexpected error occured, please contact me at nando.kools@hotmail.com and give me the error ID");
+
+            Console.Beep(500, 1200);
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            Console.ReadLine();
+            Console.ReadLine();// made 2 readlines to make sure the user doesnt skip the error.
+        }
+
+        public static void ErrorNotYetCreated()
+        {
+            Program.ErrorHandlerStart();
+            Console.WriteLine("ERROR: CODE DOES'T EXIST");
+            Console.WriteLine("ERROR ID: 0003");
+            Program.ErrorFinisher();
+            Console.WriteLine("Returning to Main Menu");
+            Program.MAINMENU();
+
         }
     }
 }
