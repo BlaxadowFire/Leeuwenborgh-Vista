b82eb1d6785005002960fff8783bc88a0b9e8068
Julean Hommel <julean.hommel204879@student.leeuwenborgh.nl>
2017-10-27 09:30:20 +0200

    added puzzle 4

---
+++ Subwaj/puzzle4.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+
+namespace Subwaj.files
+{
+    class puzzle4
+    {
+        File.ReadAllLines();
+    }
+}
