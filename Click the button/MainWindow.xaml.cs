a6d99c2f355307a8cabac18e97d7c75c1ff767ca
BlaxadowFire <nando.kools116325@student.leeuwenborgh.nl>
2017-11-21 13:36:34 +0100

    initial commit

--- Click the button/MainWindow.xaml.cs
+++ Click the button/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Speech.Synthesis;
+using System.IO;
 
 namespace Click_the_button
 {
@@ -20,9 +22,31 @@ namespace Click_the_button
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static SpeechSynthesizer Ss = new SpeechSynthesizer();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void imagejump(object sender, MouseEventArgs e)
+        {
+            Random randomlocation = new Random();
+            double dblRandomLocationH = randomlocation.Next(0, Convert.ToInt32(Height));
+            double dblRandomLocationW = randomlocation.Next(0, Convert.ToInt32(Width));
+            image.Margin = new Thickness(dblRandomLocationW, dblRandomLocationH, 0, 0);
+            if (image.Margin.Left >= (this.Width - 50) || image.Margin.Top >= (this.Height - 50) || image.Margin.Left <= 10 || image.Margin.Top <= 10)
+            {
+                image.Margin = new Thickness(40, 68, 0, 0);
+            }
+            Ss.SpeakAsyncCancelAll();
+            Ss.SpeakAsync("HAHA, can't catch me");
+        }
+
+        private void Label_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Ss.SpeakAsyncCancelAll();
+            Ss.SpeakAsync(File.ReadAllText("items/TextFile1.txt"));
         }
     }
 }
