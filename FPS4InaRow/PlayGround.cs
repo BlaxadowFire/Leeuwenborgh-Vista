ed8b4296b8a410aaef40f9d202f4627e48b73ad3
BlaxadowFire <nando.kools116325@student.leeuwenborgh.nl>
2017-11-23 14:26:56 +0100

    Added PlayGround.cs

---
+++ FPS4InaRow/PlayGround.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FPS4InaRow
+{
+    class PlayGround
+    {
+        // 7x7 multidimential array
+    }
+}
