fe7f440e25e1fadf80ebc1656e38ea785ec04e57
BlaxadowFire <nando.kools116325@student.leeuwenborgh.nl>
2017-11-23 01:15:30 +0100

    Added a start of the AI Improved the interface

--- TicTacToewpf/MainWindow.xaml.cs
+++ TicTacToewpf/MainWindow.xaml.cs
@@ -24,68 +24,76 @@ namespace TicTacToewpf
 
         public string P1 = "Soviet";
         public string P2 = "America";
+        public bool AiEnabled;
+        public bool AiMadeMove;
         public SoundPlayer MyPlayer = new SoundPlayer();
-        public bool Win;
         public int[] Turns = {0,0,0,0,0,0,0,0,0};
 
         public MainWindow()
         {
             InitializeComponent();
-            lblPlayerTurn.Content = P1;
+            ListBox.Items.Add("Highscore");
+            ListBox.Items.Add("____________________\r\n");
+            LblPlayerTurn.Content = P1;
         }
 
         public void UserInput(object sender, RoutedEventArgs e)
         {
-            if ((string)lblPlayerTurn.Content == P1)
+            ((Button)sender).Visibility = Visibility.Hidden;
+            if ((string)LblPlayerTurn.Content == P1)
             {
-                ((Image)sender).Source = new BitmapImage(new Uri("Assets/p1.png", UriKind.Relative));
-                Turns[Convertname(sender)] = 1;
+                ConvertButton(sender);
+                Turns[ConvertImage(sender)] = 1;
                 CheckWin(sender, e);
-                lblPlayerTurn.Content = P2;
+                LblPlayerTurn.Content = P2;
+                CheckAi(sender);
             }
             else
             {
-                ((Image)sender).Source = new BitmapImage(new Uri("Assets/p2.png", UriKind.Relative));
-                Turns[Convertname(sender)] = 2;
+                ConvertButton(sender);
+                Turns[ConvertImage(sender)] = 2;
                 CheckWin(sender, e);
-                lblPlayerTurn.Content = P1;
+                LblPlayerTurn.Content = P1;
+                CheckAi(sender);
             }
-            if (!Win)
+        }
+
+        public void CheckAi(object sender)
+        {
+            if (AiEnabled)
             {
-                ((Image)sender).MouseDown -= UserInput;
+                //checks if it can match win or lose condition
+
+                if (!AiMadeMove)
+                {
+                    //make use of switchcase to see what last move was.
+                    CheckLastMove(sender);
+                }
+
+
+                LblPlayerTurn.Content = (string)LblPlayerTurn.Content == P1 ? P2 : P1;
+                AiMadeMove = false;
             }
-            Win = false;
+        }
+
+        public void HelpMenu(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Click on the place you want your turn to be.");
+            MessageBox.Show("The goal is to get your flag 3 times in a row.");
+            MessageBox.Show("Press Reset in case of tie.");
         }
 
         public void ResetClick(object sender, RoutedEventArgs e)
         {
-            A.Source = new BitmapImage(new Uri("Assets/empty.png", UriKind.Relative));
-            A.MouseDown -= UserInput;
-            A.MouseDown += UserInput;
-            B.Source = new BitmapImage(new Uri("Assets/empty.png", UriKind.Relative));
-            B.MouseDown -= UserInput;
-            B.MouseDown += UserInput;
-            C.Source = new BitmapImage(new Uri("Assets/empty.png", UriKind.Relative));
-            C.MouseDown -= UserInput;
-            C.MouseDown += UserInput;
-            D.Source = new BitmapImage(new Uri("Assets/empty.png", UriKind.Relative));
-            D.MouseDown -= UserInput;
-            D.MouseDown += UserInput;
-            E.Source = new BitmapImage(new Uri("Assets/empty.png", UriKind.Relative));
-            E.MouseDown -= UserInput;
-            E.MouseDown += UserInput;
-            F.Source = new BitmapImage(new Uri("Assets/empty.png", UriKind.Relative));
-            F.MouseDown -= UserInput;
-            F.MouseDown += UserInput;
-            G.Source = new BitmapImage(new Uri("Assets/empty.png", UriKind.Relative));
-            G.MouseDown -= UserInput;
-            G.MouseDown += UserInput;
-            H.Source = new BitmapImage(new Uri("Assets/empty.png", UriKind.Relative));
-            H.MouseDown -= UserInput;
-            H.MouseDown += UserInput;
-            I.Source = new BitmapImage(new Uri("Assets/empty.png", UriKind.Relative));
-            I.MouseDown -= UserInput;
-            I.MouseDown += UserInput;
+            ImgA.Source = new BitmapImage(new Uri("Assets/empty.png", UriKind.Relative));
+            ImgB.Source = new BitmapImage(new Uri("Assets/empty.png", UriKind.Relative));
+            ImgC.Source = new BitmapImage(new Uri("Assets/empty.png", UriKind.Relative));
+            ImgD.Source = new BitmapImage(new Uri("Assets/empty.png", UriKind.Relative));
+            ImgE.Source = new BitmapImage(new Uri("Assets/empty.png", UriKind.Relative));
+            ImgF.Source = new BitmapImage(new Uri("Assets/empty.png", UriKind.Relative));
+            ImgG.Source = new BitmapImage(new Uri("Assets/empty.png", UriKind.Relative));
+            ImgH.Source = new BitmapImage(new Uri("Assets/empty.png", UriKind.Relative));
+            ImgI.Source = new BitmapImage(new Uri("Assets/empty.png", UriKind.Relative));
             
             /*
             foreach (int turn in turns)
@@ -97,9 +105,17 @@ namespace TicTacToewpf
             {
                 Turns[i] = 0;
             }
-
-            lblPlayerTurn.Content = P1;
-            Win = true;
+            {
+                BtnA.Visibility = Visibility.Visible;
+                BtnB.Visibility = Visibility.Visible;
+                BtnC.Visibility = Visibility.Visible;
+                BtnD.Visibility = Visibility.Visible;
+                BtnE.Visibility = Visibility.Visible;
+                BtnF.Visibility = Visibility.Visible;
+                BtnG.Visibility = Visibility.Visible;
+                BtnH.Visibility = Visibility.Visible;
+                BtnI.Visibility = Visibility.Visible;
+            }
         }
 
         public void CheckWin(object sender, RoutedEventArgs e)
@@ -108,58 +124,59 @@ namespace TicTacToewpf
                 Turns[0] == Turns[3] && Turns[3] == Turns[6] && Turns[0] != 0 || Turns[1] == Turns[4] && Turns[4] == Turns[7] && Turns[1] != 0 || Turns[2] == Turns[5] && Turns[5] == Turns[8] && Turns[2] != 0 ||
                 Turns[0] == Turns[4] && Turns[4] == Turns[8] && Turns[0] != 0 || Turns[2] == Turns[4] && Turns[4] == Turns[6] && Turns[2] != 0)
             {
-                if ((string)lblPlayerTurn.Content == P1)
+                ListBox.Items.Add(LblPlayerTurn.Content);
+                if ((string)LblPlayerTurn.Content == P1)
                 {
                     MyPlayer.SoundLocation = "Assets/soviet.wav";
                     MyPlayer.Play();
                 }
-                MessageBox.Show(lblPlayerTurn.Content + " Won");
+                MessageBox.Show(LblPlayerTurn.Content + " Won");
                 MyPlayer.Stop();
                 ResetClick(sender, e);
             }
             else
             {
-                lblPlayerTurn.Content = (string)lblPlayerTurn.Content == P1 ? P2 : P1;
+                LblPlayerTurn.Content = (string)LblPlayerTurn.Content == P1 ? P2 : P1;
             }
         }
 
-        public int Convertname(object sender)
+        public int ConvertImage(object sender)
         {
-            switch (((Image)sender).Name)
+            switch (((Button)sender).Name)
             {
-                case "A":
+                case "BtnA":
                     {
                         return 0;
                     }
-                case "B":
+                case "BtnB":
                     {
                         return 1;
                     }
-                case "C":
+                case "BtnC":
                     {
                         return 2;
                     }
-                case "D":
+                case "BtnD":
                     {
                         return 3;
                     }
-                case "E":
+                case "BtnE":
                     {
                         return 4;
                     }
-                case "F":
+                case "BtnF":
                     {
                         return 5;
                     }
-                case "G":
+                case "BtnG":
                     {
                         return 6;
                     }
-                case "H":
+                case "BtnH":
                     {
                         return 7;
                     }
-                case "I":
+                case "BtnI":
                     {
                         return 8;
                     }
@@ -168,6 +185,158 @@ namespace TicTacToewpf
                         return -1;
                     }
             }
+        }
+        public void ConvertButton(object sender)
+        {
+            if ((string)LblPlayerTurn.Content == P1)
+            {
+                switch (((Button) sender).Name)
+                {
+                    case "BtnA":
+                    {
+                        ImgA.Source = new BitmapImage(new Uri("Assets/p1.png", UriKind.Relative));
+                        break;
+                    }
+                    case "BtnB":
+                    {
+                        ImgB.Source = new BitmapImage(new Uri("Assets/p1.png", UriKind.Relative));
+                        break;
+                    }
+                    case "BtnC":
+                    {
+                        ImgC.Source = new BitmapImage(new Uri("Assets/p1.png", UriKind.Relative));
+                        break;
+                    }
+                    case "BtnD":
+                    {
+                        ImgD.Source = new BitmapImage(new Uri("Assets/p1.png", UriKind.Relative));
+                        break;
+                    }
+                    case "BtnE":
+                    {
+                        ImgE.Source = new BitmapImage(new Uri("Assets/p1.png", UriKind.Relative));
+                        break;
+                    }
+                    case "BtnF":
+                    {
+                        ImgF.Source = new BitmapImage(new Uri("Assets/p1.png", UriKind.Relative));
+                        break;
+                    }
+                    case "BtnG":
+                    {
+                        ImgG.Source = new BitmapImage(new Uri("Assets/p1.png", UriKind.Relative));
+                        break;
+                    }
+                    case "BtnH":
+                    {
+                        ImgH.Source = new BitmapImage(new Uri("Assets/p1.png", UriKind.Relative));
+                        break;
+                    }
+                    case "BtnI":
+                    {
+                        ImgI.Source = new BitmapImage(new Uri("Assets/p1.png", UriKind.Relative));
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                switch (((Button) sender).Name)
+                {
+                    case "BtnA":
+                    {
+                        ImgA.Source = new BitmapImage(new Uri("Assets/p2.png", UriKind.Relative));
+                        break;
+                    }
+                    case "BtnB":
+                    {
+                        ImgB.Source = new BitmapImage(new Uri("Assets/p2.png", UriKind.Relative));
+                        break;
+                    }
+                    case "BtnC":
+                    {
+                        ImgC.Source = new BitmapImage(new Uri("Assets/p2.png", UriKind.Relative));
+                        break;
+                    }
+                    case "BtnD":
+                    {
+                        ImgD.Source = new BitmapImage(new Uri("Assets/p2.png", UriKind.Relative));
+                        break;
+                    }
+                    case "BtnE":
+                    {
+                        ImgE.Source = new BitmapImage(new Uri("Assets/p2.png", UriKind.Relative));
+                        break;
+                    }
+                    case "BtnF":
+                    {
+                        ImgF.Source = new BitmapImage(new Uri("Assets/p2.png", UriKind.Relative));
+                        break;
+                    }
+                    case "BtnG":
+                    {
+                        ImgG.Source = new BitmapImage(new Uri("Assets/p2.png", UriKind.Relative));
+                        break;
+                    }
+                    case "BtnH":
+                    {
+                        ImgH.Source = new BitmapImage(new Uri("Assets/p2.png", UriKind.Relative));
+                        break;
+                    }
+                    case "BtnI":
+                    {
+                        ImgI.Source = new BitmapImage(new Uri("Assets/p2.png", UriKind.Relative));
+                        break;
+                    }
+                }
+            }
+        }
+
+        public void CheckLastMove(object sender)
+        {
+            switch (ConvertImage(sender))
+            {
+                case 0:
+                case 4:
+                {
+                    //8
+                    break;
+                }
+                case 2:
+                {
+                        //6
+                    break;
+                }
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                {
+                        //5
+                    break;
+                }
+                case 6:
+                {
+                        //2
+                    break;
+                }
+
+                case 8:
+                {
+                        //0
+                    break;
+                }
+            }
+        }
+        public void SwitchAi(object sender, RoutedEventArgs e)
+        {
+            AiEnabled = !AiEnabled;
+            BtnSwitchAi.Background = AiEnabled ? Brushes.Green : Brushes.Red;
+        }
+
+        public void Ai()
+        {
+
         }
     }
 }
