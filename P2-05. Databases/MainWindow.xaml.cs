6a8bc5852ebee2ae92377b784fe50e6ab3b18256
Kortman <31918988+mrKortman@users.noreply.github.com>
2017-12-13 12:47:44 +0100

    Initial commit, 05Databases + 06. Query's handout

--- P2-05. Databases/MainWindow.xaml.cs
+++ P2-05. Databases/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace P2_05.Databases
 {
@@ -20,9 +23,33 @@ namespace P2_05.Databases
     /// </summary>
     public partial class MainWindow : Window
     {
+        ConsoleDataSet datasource = new ConsoleDataSet();
+
         public MainWindow()
         {
             InitializeComponent();
+            FillDataGrid();
+        }
+
+        private void FillDataGrid()
+        {
+            string strConnection = ConfigurationManager.ConnectionStrings["consoleConnection"].ConnectionString;
+            string CmdString = string.Empty;
+
+            using (SqlConnection sqlConnection = new SqlConnection(strConnection))
+            {
+                CmdString = "SELECT * FROM Games";
+                SqlCommand cmd = new SqlCommand(CmdString, sqlConnection);
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                DataTable dt = datasource.Tables["Games"];
+                sda.Fill(dt);
+                myDataGrid.ItemsSource = dt.DefaultView;
+            }
+        }
+
+        private void SaveData()
+        {
+            //datasourc
         }
     }
 }
--- P2-05. Databases/Properties/Settings.Designer.cs
+++ P2-05. Databases/Properties/Settings.Designer.cs
@@ -8,22 +8,28 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace P2_05.Databases.Properties
-{
-
-
+namespace P2_05.Databases.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.5.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=lo21225;Initial Catalog=Console;Integrated Security=True")]
+        public string ConsoleConnectionString {
+            get {
+                return ((string)(this["ConsoleConnectionString"]));
             }
         }
     }
