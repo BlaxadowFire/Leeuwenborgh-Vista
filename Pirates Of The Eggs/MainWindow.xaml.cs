749aca965fd0e6efc968df3722c7bea4cf7d7c7f
BlaxadowFire <nando.kools116325@student.leeuwenborgh.nl>
2018-01-10 09:52:16 +0100

    Referenties toegevoegd

--- Pirates Of The Eggs/MainWindow.xaml.cs
+++ Pirates Of The Eggs/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Pirates_Of_The_Eggs
 {
