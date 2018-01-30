83c80ea82aff1c9f545ebfb52c7109df4c4c04b9
Maurits Annema <maurits.annema202141@student.leeuwenborgh.nl>
2018-01-30 11:36:50 +0100

    button colors

--- Pirates Of The Eggs/Pages/Main.xaml.cs
+++ Pirates Of The Eggs/Pages/Main.xaml.cs
@@ -115,5 +115,10 @@ namespace Pirates_Of_The_Eggs
         {
             MainWindow.MainFrame.Navigate(new Main());
         }
+
+        private void TXTCheck_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
