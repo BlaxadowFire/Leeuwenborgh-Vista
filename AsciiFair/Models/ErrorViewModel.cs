0d3d6171e606637c0657e490597875035ecb1910
BlaxadowFire <nando.kools116325@student.leeuwenborgh.nl>
2019-04-08 13:54:16 +0200

    Add project files.

---
+++ AsciiFair/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AsciiFair.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
