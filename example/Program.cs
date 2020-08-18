10deb0e03e3307ec4e9a409597b4ff08c423d4f2
BlaxadowFire <nando.kools116325@student.leeuwenborgh.nl>
2020-08-18 18:40:50 +0200

    initial commit.

---
+++ example/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example
+{
+    class Program
+    {
+        public static int intResult;
+        static int ShowInfo()
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            return x;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dit is een uitleg over functies");
+            Program.intResult = Program.ShowInfo();
+            Console.WriteLine("uitkomst is" + intResult);
+            Console.ReadLine();
+        }
+    }
+}
