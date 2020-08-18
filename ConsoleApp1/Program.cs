ca7ba0782ca8f14cae6ff74431b9c92e7e17f719
BlaxadowFire <nando.kools116325@student.leeuwenborgh.nl>
2020-08-18 17:06:13 +0200

    no message

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,28 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Wie is de meest awesome persoon die er bestaat\r\n" +
+                              "1). Nando \r\n" +
+                              "2). Denzel \r\n" +
+                              "3). Collin");
+            string x = Console.ReadLine();
+            if (x == "1")
+            {
+                Console.WriteLine("Klopt!");
+            }
+            else if (x == "2")
+            {
+                Console.WriteLine("Close enough!");
+            }
+            else if (x == "3")
+            {
+                Console.WriteLine("Meh");
+            }
+            else
+            {
+                Console.WriteLine("Je hebt een onjuiste keuze gegeven.");
+            }
+            Console.ReadLine();
         }
     }
 }
