1886d9f229e68fbd73085905733b932d99eff63b
BlaxadowFire <nando.kools116325@student.leeuwenborgh.nl>
2020-08-23 21:34:14 +0200

    Added actual code

--- Thread Test/Program.cs
+++ Thread Test/Program.cs
@@ -3,13 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Thread_Test
 {
     class Program
     {
+        public static int i = 37;
         static void Main(string[] args)
         {
+            ThreadStart ts = new ThreadStart(Background);
+            Thread BackgroundThread = new Thread(ts);
+            BackgroundThread.Start();
+
+            do
+            {
+                i++;
+                Console.WriteLine("BEEEEEEEEEEEEP" + i);
+                if (i == 30000)
+                {
+                    i = 37;
+                }
+
+            }
+            while (true);
+        }
+        public static void Background()
+        {
+            do
+            {
+                Console.Beep(i, 200);
+                Console.Beep(i, 200);
+                Console.Beep(i, 200);
+                Console.Beep(i, 200);
+            }
+            while (true);
         }
     }
 }
