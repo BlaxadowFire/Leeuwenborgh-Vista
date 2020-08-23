e4cb4273edffe3a99df5be65f3c52c03cee0becc
BlaxadowFire <nando.kools116325@student.leeuwenborgh.nl>
2020-08-23 21:33:11 +0200

    Added actual code

--- RiotApiApp/MainWindow.xaml.cs
+++ RiotApiApp/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using RiotSharp;
+using RiotSharp.Misc;
 
 namespace RiotApiApp
 {
@@ -20,9 +22,95 @@ namespace RiotApiApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static long lngSumID;
+        public static Region region;
+        public static RiotApi api = RiotApi.GetDevelopmentInstance("RGAPI-5f74277e-2c41-4705-9b0a-a494b2bd6660");
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                region = CheckRegion();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("please select a region");
+            }
+                try
+            { 
+                var summoner = api.GetSummonerByName(region, SummonerNameTxtBox.Text);
+                lngSumID = summoner.Id;
+                MessageBox.Show(summoner.Id.ToString());
+                Spectator.CurrentGameInfo();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Summoner does not exist in that region, please try again");
+            }
+        }
+        private Region CheckRegion()
+        {
+            ComboBoxItem cbi = ((ComboBoxItem)RegionCBox.SelectedValue);
+            switch (cbi.Content.ToString())
+            {
+                case "br":
+                    {
+                        return Region.br;
+                    }
+                case "eune":
+                    {
+                        return Region.eune;
+                    }
+                case "euw":
+                    {
+                        return Region.euw;
+                    }
+                case "global":
+                    {
+                        return Region.global;
+                    }
+                case "jp":
+                    {
+                        return Region.jp;
+                    }
+                case "kr":
+                    {
+                        return Region.kr;
+                    }
+                case "lan":
+                    {
+                        return Region.lan;
+                    }
+                case "las":
+                    {
+                        return Region.las;
+                    }
+                case "na":
+                    {
+                        return Region.na;
+                    }
+                case "oce":
+                    {
+                        return Region.oce;
+                    }
+                case "ru":
+                    {
+                        return Region.ru;
+                    }
+                case "tr":
+                    {
+                        return Region.tr;
+                    }
+                default:
+                    {
+                        return Region.global;
+                    }
+            }
         }
     }
 }
---
+++ RiotApiApp/Spectator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RiotSharp;
+
+namespace RiotApiApp
+{
+    class Spectator
+    {
+        public static long ChampID;
+        public static void CurrentGameInfo()
+        {
+            var CurrentChampion = MainWindow.api.GetCurrentGame(MainWindow.region, MainWindow.lngSumID);
+            foreach (RiotSharp.SpectatorEndpoint.Participant Summoner in CurrentChampion.Participants)
+            {
+                if (Summoner.SummonerId == MainWindow.lngSumID)
+                {
+                    ChampID = Summoner.ChampionId;
+                }
+            }
+        }
+    }
+}
